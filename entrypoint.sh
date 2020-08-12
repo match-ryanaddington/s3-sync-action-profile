d521d1bc23a00016791e034bd0af54d8e3cf606f
ryan.addington <ryan.addington@match.com>
2020-08-12 16:54:00 -0500

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,13 +41,13 @@ sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
 
-# Clear out credentials after we're done.
-# We need to re-run `aws configure` with bogus input instead of
-# deleting ~/.aws in case there are other credentials living there.
-# https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
+# # Clear out credentials after we're done.
+# # We need to re-run `aws configure` with bogus input instead of
+# # deleting ~/.aws in case there are other credentials living there.
+# # https://forums.aws.amazon.com/thread.jspa?threadID=148833
+# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+# null
+# null
+# null
+# text
+# EOF
