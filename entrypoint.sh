05142b768c05d6b638d5af8bb9b51292d54c3d11
ryan.addington <ryan.addington@match.com>
2020-08-12 16:59:36 -0500

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,10 +30,12 @@ fi
 # # Create a dedicated profile for this action to avoid conflicts
 # # with past/future actions.
 # # https://github.com/jakejarvis/s3-sync-action/issues/1
-# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-# ${AWS_REGION}
-# text
-# EOF
+aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+
+
+${AWS_REGION}
+text
+EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
