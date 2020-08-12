88f0b167f84ff2f0a9bb0977168329a9a6153860
ryan.addington <ryan.addington@match.com>
2020-08-12 16:56:08 -0500

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,9 +37,7 @@ fi
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-              --no-progress \
-              ${ENDPOINT_APPEND} $*"
+sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} $*"
 
 # # Clear out credentials after we're done.
 # # We need to re-run `aws configure` with bogus input instead of
